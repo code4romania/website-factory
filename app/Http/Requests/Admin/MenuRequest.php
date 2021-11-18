ef0dadabac53f7496a21df46bebbbc867707b027
Andrei Ioniță <hi@andrei.io>
2021-11-18 20:27:04 +0000

    fix: nested attribute validation

--- app/Http/Requests/Admin/MenuRequest.php
+++ app/Http/Requests/Admin/MenuRequest.php
@@ -7,6 +7,7 @@ namespace App\Http\Requests\Admin;
 use App\Models\MenuItem;
 use App\Services\TranslatableFormRequestRules;
 use Illuminate\Foundation\Http\FormRequest as BaseRequest;
+use Illuminate\Support\Str;
 
 class MenuRequest extends BaseRequest
 {
@@ -27,14 +28,28 @@ class MenuRequest extends BaseRequest
      */
     public function rules(): array
     {
-        return TranslatableFormRequestRules::make(MenuItem::class, [
-            'items'                               => ['array'],
-            'items.*.label'                       => ['required', 'string'],
-            'items.*.type'                        => ['required', 'string'],
-            'items.*.children.*.label'            => ['required', 'string', 'sometimes'],
-            'items.*.children.*.type'             => ['required', 'string', 'sometimes'],
-            'items.*.children.*.children.*.label' => ['required', 'string', 'sometimes'],
-            'items.*.children.*.children.*.type'  => ['required', 'string', 'sometimes'],
-        ]);
+        return TranslatableFormRequestRules::make(MenuItem::class, $this->nestedRules(2));
+    }
+
+    protected function nestedRules(int $depth): array
+    {
+        $template = [
+            '%prefix%'                => ['array'],
+            '%prefix%.*.label'        => ['required', 'string'],
+            '%prefix%.*.type'         => ['required', 'string'],
+            '%prefix%.*.external_url' => ['nullable', 'url'],
+        ];
+
+        $rules = collect();
+
+        for ($level = 0; $level <= $depth; $level++) {
+            $prefix = 'items' . \str_repeat('.*.children', $level);
+
+            foreach ($template as $key => $value) {
+                $rules->put(Str::replace('%prefix%', $prefix, $key), $value);
+            }
+        }
+
+        return $rules->all();
     }
 }
--- app/Services/TranslatableFormRequestRules.php
+++ app/Services/TranslatableFormRequestRules.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Illuminate\Support\Str;
+
 class TranslatableFormRequestRules
 {
     public static function make(string $model, array $input): array
@@ -14,12 +16,9 @@ class TranslatableFormRequestRules
 
         foreach ($input as $key => $rule) {
             // Check for nested array keys
-            $parts = \explode('.', $key);
-            if (count($parts) > 1) {
-                $key = end($parts);
-            }
+            $attribute = Str::of($key)->explode('.')->last();
 
-            if ($model->isTranslatableAttribute($key)) {
+            if ($model->isTranslatableAttribute($attribute)) {
                 locales()->each(fn ($locale) => $rules->put("$key.$locale", $rule));
             } else {
                 $rules->put($key, $rule);
