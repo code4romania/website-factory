33fb83de609af9358a4ecb3e3c2b517d530a88c4
Andrei Ioniță <hi@andrei.io>
2021-12-04 19:07:13 +0000

    chore: sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -40,4 +40,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
 }
