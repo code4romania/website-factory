c25e70e51528a24fa55846353d31844835dbb373
Andrei Ioniță <hi@andrei.io>
2022-04-11 14:51:36 +0100

    wip

--- resources/js/components/Blocks/BlockRepeater.vue
+++ resources/js/components/Blocks/BlockRepeater.vue
@@ -24,7 +24,17 @@
         </template>
 
         <template #footer>
-            <button type="button" @click="addBlock">Add another</button>
+            <div>
+                <button
+                    type="button"
+                    @click="addBlock"
+                    class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 focus:ring-offset-gray-100"
+                >
+                    <icon name="System/add-line" class="w-5 h-5 mr-2 -ml-1" />
+
+                    <span v-text="$t('app.action.add')" />
+                </button>
+            </div>
         </template>
     </draggable>
 </template>
