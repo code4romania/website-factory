6626c62a1b37df1718993cd207f24000e123a599
Andrei Ioniță <hi@andrei.io>
2022-05-07 15:01:16 +0100

    chore: cleanup

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,6 +68,7 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'visibility' => env('AWS_BUCKET_DEFAULT_VISIBILITY', 'private'),
+            'throw' => true,
         ],
 
     ],
