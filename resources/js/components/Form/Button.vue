94c4176800171f41aebd47a09d0f59f6c304f2c3
Andrei Ioniță <hi@andrei.io>
2022-05-23 09:45:49 +0100

    feat: form button colors

--- resources/js/components/Form/Button.vue
+++ resources/js/components/Form/Button.vue
@@ -1,11 +1,8 @@
 <template>
     <button
         :type="type"
-        class="relative inline-flex items-center justify-center font-semibold tracking-wider transition duration-150 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2"
-        :class="[
-            size,
-            'text-white bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 disabled:opacity-50 disabled:cursor-default',
-        ]"
+        class="relative inline-flex items-center justify-center font-semibold tracking-wider transition duration-150 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-default"
+        :class="[size, color]"
     >
         <slot>{{ label }}</slot>
     </button>
@@ -30,6 +27,12 @@
                 default: 'md',
                 validator: (size) => ['xs', 'sm', 'md', 'lg', 'xl'].includes(size),
             },
+            color: {
+                type: String,
+                default: 'black',
+                validator: (color) =>
+                    ['green', 'red', 'blue', 'black', 'white'].includes(color),
+            },
         },
         setup(props) {
             const size = computed(
@@ -43,8 +46,20 @@
                     }[props.size])
             );
 
+            const color = computed(
+                () =>
+                    ({
+                        green: `text-white bg-green-600 hover:bg-green-700 focus:ring-green-500`,
+                        red: `text-white bg-red-600 hover:bg-red-700 focus:ring-red-500`,
+                        blue: `text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500`,
+                        black: `text-white bg-gray-800 hover:bg-gray-700 focus:ring-blue-500`,
+                        white: `border-gray-300 text-gray-700 bg-white hover:bg-gray-50 focus:ring-blue-500`,
+                    }[props.color])
+            );
+
             return {
                 size,
+                color,
             };
         },
     };
