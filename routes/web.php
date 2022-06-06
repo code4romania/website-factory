2c068cd5f68f7f5c5870cd3fc18e552a2bedcd12
Andrei Ioniță <hi@andrei.io>
2022-06-06 19:29:50 +0100

    fix: improve theme cache busting
    closes #32

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/theme/style.css', Front\ThemeController::class)->name('theme')
     ->withoutMiddleware('web')
-    ->middleware('cache.headers:public;max_age=2628000;etag');
+    ->middleware('cache.headers:public;etag');
 
 Route::prefix('{locale?}')->group(function () {
     if (Features::hasDecisions()) {
