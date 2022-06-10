178f712c6aa04aa5ed7bf63631284de5952e821c
Andrei Ioniță <hi@andrei.io>
2022-06-10 17:31:18 +0100

    chore: update dependencies

--- resources/themes/public.tailwind.config.js
+++ resources/themes/public.tailwind.config.js
@@ -14,6 +14,7 @@ const colorVariable = (variable) => {
     };
 };
 
+/** @type {import('tailwindcss').Config} */
 module.exports = merge(require('../../tailwind.config.js'), {
     theme: {
         extend: {
