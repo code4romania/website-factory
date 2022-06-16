dcb9b5e31add83b02581ed709c2a41fcaabd6aa1
Andrei Ioniță <hi@andrei.io>
2022-06-16 21:03:38 +0300

    fix: add missing translations

--- resources/views/components/site/code4.blade.php
+++ resources/views/components/site/code4.blade.php
@@ -3,13 +3,13 @@
         <x-icon-code4 class="h-6" />
 
         <div class="ml-6 text-sm font-medium text-gray-700">
-            <span>@lang('banner.code4')</span>
+            <span>@lang('app.banner.code4')</span>
             <a
                 href="https://www.code4.ro/"
                 target="_blank"
                 rel="noopener"
                 class="text-blue-600 hover:underline whitespace-nowrap">
-                @lang('banner.more')
+                @lang('app.banner.more')
             </a>
         </div>
     </div>
--- resources/views/front/search/results.blade.php
+++ resources/views/front/search/results.blade.php
@@ -37,7 +37,7 @@
                         <div class="mt-4 text-right">
                             <a href="{{ $item->search_result->url_public }}"
                                 class="text-sm font-semibold text-primary hover:underline">
-                                @lang('banner.more') <span aria-hidden="true">&rarr;</span>
+                                @lang('app.banner.more') <span aria-hidden="true">&rarr;</span>
                             </a>
                         </div>
                     </div>
