dca14ecc6444855cdf1a593e58f0e4ec27a2792b
Andrei Ioniță <hi@andrei.io>
2022-07-06 18:35:56 +0100

    revert: separate front locale middleware

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/theme/style.css', Front\ThemeController::class)->name('theme')
     ->withoutMiddleware('web')
     ->middleware('cache.headers:public;etag');
 
-Route::prefix('{locale?}')->middleware('locale')->group(function () {
+Route::prefix('{locale?}')->group(function () {
     if (Features::hasDecisions()) {
         Route::get('/decisions', [Front\DecisionController::class, 'index'])->name('decisions.index');
         Route::get('/decisions/category', [Front\DecisionCategoryController::class, 'index'])->name('decision_categories.index');
