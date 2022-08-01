0cbf11dc01ad4f6956a7e6dc638fadfa18da0efc
Andrei Ioniță <hi@andrei.io>
2022-08-01 19:32:44 +0100

    fix: block title color

--- resources/views/components/blocks/_title.blade.php
+++ resources/views/components/blocks/_title.blade.php
@@ -3,7 +3,7 @@
 ])
 
 @if ($title)
-    <h1 {{ $attributes->merge(['class' => 'mb-8 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl']) }}>
+    <h1 {{ $attributes->merge(['class' => 'mb-8 text-2xl font-bold sm:text-3xl md:text-4xl']) }}>
         {{ $title }}
     </h1>
 @endif
