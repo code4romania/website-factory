91ecde827795e74c8f70f8d7567a3713c9330c3e
Andrei Ioniță <hi@andrei.io>
2022-08-02 08:57:18 +0100

    build: change init order

--- .docker/s6-rc.d/laravel/init.sh
+++ .docker/s6-rc.d/laravel/init.sh
@@ -5,16 +5,16 @@ cd /var/www
 echo "Laravel init started"
 php artisan down --render="errors::503"
 
-php artisan storage:link
+php artisan migrate --force
+php artisan wf:install
+php artisan wf:translations
+php artisan wf:sequences
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
 php artisan icons:cache
-php artisan migrate --force
-php artisan wf:install
-php artisan wf:translations
-php artisan wf:sequences
+php artisan storage:link
 
 echo "Laravel init done"
 php artisan about
