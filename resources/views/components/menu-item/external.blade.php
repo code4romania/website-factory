5e8ab59f55ed28a879088b1975ffaf6687ef5a77
Andrei Ioniță <hi@andrei.io>
2022-09-06 14:51:48 +0100

    fix: handle incomplete menu items

--- resources/views/components/menu-item/external.blade.php
+++ resources/views/components/menu-item/external.blade.php
@@ -1,11 +1,13 @@
 @props(['item', 'inactiveClass' => '', 'activeClass' => ''])
 
-<a
-    {{ $attributes->merge([
-        'href' => $item->url,
-        'target' => '_blank',
-        'rel' => 'noopener',
-        'class' => $inactiveClass,
-    ]) }}>
-    {{ $item->label }}
-</a>
+@if ($item->url)
+    <a
+        {{ $attributes->merge([
+            'href' => $item->url,
+            'target' => '_blank',
+            'rel' => 'noopener',
+            'class' => $inactiveClass,
+        ]) }}>
+        {{ $item->label }}
+    </a>
+@endif
