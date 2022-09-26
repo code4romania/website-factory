7f3fbd520ba84674d694c3e0d1759c1460ac38d6
Andrei Ioniță <hi@andrei.io>
2022-09-26 13:10:16 +0100

    fix: reduce footer margin

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -42,7 +42,7 @@
 
     <x-site.notice />
 
-    <main id="content" class="flex-1 mb-16 sm:mb-24 lg:mb-32">
+    <main id="content" class="flex-1 mb-12 lg:mb-16">
         {{ $slot }}
     </main>
 
