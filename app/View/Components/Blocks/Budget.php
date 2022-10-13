9fef03ccc4b268c3ea0e3122f8f80dba7dcb5a66
Andrei Ioniță <hi@andrei.io>
2022-10-13 14:28:25 +0100

    wip

--- app/View/Components/Blocks/Budget.php
+++ app/View/Components/Blocks/Budget.php
@@ -6,12 +6,20 @@ namespace App\View\Components\Blocks;
 
 class Budget extends BlockComponent
 {
+    public ?string $title;
+
+    public ?string $html;
+
     public array $chartData = [];
 
     public string $dataKey;
 
     public function setup(): void
     {
+        $this->title = $this->block->translatedInput('title');
+
+        $this->html = $this->block->translatedInput('text');
+
         $this->dataKey = 'budgetData' . $this->id;
 
         $this->chartData = $this->block->input('data');
--- resources/views/components/blocks/budget.blade.php
+++ resources/views/components/blocks/budget.blade.php
@@ -1,7 +1,13 @@
-<script>
-    var {{ $dataKey }} = @json($chartData);
-</script>
+<x-blocks._title :title="$title" />
 
-<div class="p-4 bg-zinc-100">
+<div class="prose prose-primary md:prose-lg">
+    {!! $html !!}
+</div>
+
+<div class="p-4 mt-8 bg-zinc-100">
+    <script>
+        var {{ $dataKey }} = @json($chartData);
+    </script>
+
     <div x-data x-budget="{{ $dataKey }}" class="w-full max-h-screen aspect-[2/3] sm:aspect-[3/2]"></div>
 </div>
