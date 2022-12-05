08862d66073b586e8f04f04e1fa104e5e296bfea
Andrei Ioniță <hi@andrei.io>
2022-12-05 17:46:22 +0000

    fix: form submission empty field display

--- resources/js/pages/Forms/Submission.vue
+++ resources/js/pages/Forms/Submission.vue
@@ -16,29 +16,30 @@
             },
         ]"
     >
-        <div class="bg-white shadow shrink-0">
-            <div class="relative sm:p-0">
-                <dl class="sm:divide-y sm:divide-gray-100">
-                    <div
-                        v-for="(row, index) in data"
-                        :key="index"
-                        class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4"
-                        :class="{
-                            'bg-gray-50': index % 2 !== 0,
-                        }"
+        <dl
+            class="relative bg-white shadow shrink-0 sm:divide-y sm:divide-gray-100"
+        >
+            <div
+                v-for="(row, index) in data"
+                :key="index"
+                class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 even:bg-gray-50"
+            >
+                <dt
+                    class="text-sm font-medium text-gray-500"
+                    v-text="row.label"
+                />
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    <span
+                        v-if="[null, '&amp;mdash;'].includes(row.value)"
+                        class="text-gray-500"
                     >
-                        <dt
-                            class="text-sm font-medium text-gray-500"
-                            v-text="row.label"
-                        />
-                        <dd
-                            class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
-                            v-text="row.value"
-                        />
-                    </div>
-                </dl>
+                        &mdash;
+                    </span>
+
+                    <span v-else v-text="row.value" />
+                </dd>
             </div>
-        </div>
+        </dl>
     </layout>
 </template>
 
