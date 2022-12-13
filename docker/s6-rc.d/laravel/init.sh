399187573d0f58d544469dc5bfdd66c2f20d5496
Andrei Ioniță <hi@andrei.io>
2022-12-13 15:23:25 +0000

    build: pass container environment to php-fpm (#152)
    * build: pass container environment to php-fpm
    
    * build: remove unused cron service
    
    * build: change init commands order

--- docker/s6-rc.d/laravel/init.sh
+++ docker/s6-rc.d/laravel/init.sh
@@ -2,19 +2,15 @@
 
 cd /var/www
 
-echo "Laravel init started"
-php artisan down
+php artisan migrate --force
+php artisan wf:setup
+php artisan wf:sequences
+php artisan storage:link
 
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
 php artisan icons:cache
-php artisan storage:link
-php artisan migrate --force
-php artisan wf:setup
-php artisan wf:sequences
 
-echo "Laravel init done"
-php artisan up
 php artisan about
