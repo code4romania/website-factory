d5fc04e2f0cd8c07191db8f602b30f7c4d7bbe93
Andrei Ioniță <hi@andrei.io>
2023-03-01 20:29:00 +0000

    fix: banner text (#290)

--- resources/views/components/site/banner-government.blade.php
+++ resources/views/components/site/banner-government.blade.php
@@ -3,9 +3,8 @@
         href="https://code4.ro"
         target="_blank"
         rel="noopener"
-        class="flex items-center w-auto gap-3 text-sm text-white/60 hover:text-white focus-visible:text-white focus:outline-0 hover:underline focus:underline"
+        class="flex items-center w-full max-w-2xl gap-3 text-sm text-white/60 hover:text-white focus-visible:text-white focus:outline-0 hover:underline focus:underline"
     >
-
         <span>{{ $text }}</span>
 
         <x-icon-code4 class="h-6 sm:h-8 shrink-0 fill-white/80" />
