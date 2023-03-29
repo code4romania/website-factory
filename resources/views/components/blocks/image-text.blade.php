86a9eb213bbcd1abaedbb4eba204ceba16a94ff2
Andrei Ioniță <hi@andrei.io>
2023-03-29 17:38:29 +0100

    feat: component work (#291)
    * feat: add buttons to image with text block
    
    * fix: remove category description max-width
    
    * fix: reduce person title font size

--- resources/views/components/blocks/image-text.blade.php
+++ resources/views/components/blocks/image-text.blade.php
@@ -1,15 +1,15 @@
-<div
-    @class([
-        'grid gap-8 grid-flow-row-dense',
-        $containerColumns(),
-        $containerAlign(),
-    ])>
+<div @class([
+    'grid gap-8 grid-flow-row-dense',
+    $containerColumns(),
+    $containerAlign(),
+])>
     @if ($image)
         <x-media.figure
             :class="$imageColumns()"
             :src="$image->getUrl()"
             :caption="$image->caption"
-            :preload="$shouldPreload()" />
+            :preload="$shouldPreload()"
+        />
     @endif
 
     <div @class([$contentColumns()])>
@@ -17,6 +17,27 @@
 
         <div class="prose prose-lg prose-teal">
             {!! $html !!}
+        </div>
+
+        <div class="flex items-center mt-10 gap-x-3">
+            @if ($primary_button_url && $primary_button_text)
+                <a
+                    href="{{ $primary_button_url }}"
+                    class="inline-flex rounded-md bg-primary/10 px-3.5 py-2.5 text-sm font-semibold text-primary shadow-sm hover:bg-primary/5 border border-primary/25"
+                >
+                    {{ $primary_button_text }}
+                </a>
+            @endif
+
+            @if ($secondary_button_url && $secondary_button_text)
+                <a
+                    href="{{ $secondary_button_url }}"
+                    class="inline-flex items-center text-sm px-3.5 py-2.5 rounded-md font-semibold text-gray-900 gap-x-1 hover:bg-gray-50 border border-transparent"
+                >
+                    {{ $secondary_button_text }}
+                    <x-ri-arrow-right-line class="w-4 h-4" />
+                </a>
+            @endif
         </div>
     </div>
 </div>
