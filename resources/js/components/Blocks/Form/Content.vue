49301a37f98710050d97bab55e9d5550c7be8ca9
Andrei Ioniță <hi@andrei.io>
2023-04-20 18:18:19 +0100

    feat: add form content blocks

---
+++ resources/js/components/Blocks/Form/Content.vue
@@ -0,0 +1,25 @@
+<template>
+    <localized-field
+        field="form-input"
+        :label="$t('field.title')"
+        v-model="content.title"
+    />
+
+    <localized-field
+        field="form-editor"
+        :label="$t('field.text')"
+        v-model="content.text"
+    />
+</template>
+
+<script>
+    import { defineFormBlock } from '@/helpers';
+
+    export default defineFormBlock({
+        type: 'content',
+        fields: {
+            title: Object,
+            text: Object,
+        },
+    });
+</script>
