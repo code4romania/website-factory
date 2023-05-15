e635c714a460588d6fe131bd6ff71598420b5779
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-05-15 23:44:36 +0300

    sarma pentru crestem ong... @andreiio Olivia a zis

--- resources/views/components/site/partners.blade.php
+++ resources/views/components/site/partners.blade.php
@@ -1,6 +1,8 @@
 <aside class="border-b">
     <div class="container flex items-center gap-4 sm:justify-end md:gap-x-8">
-        <span class="text-sm font-medium text-gray-600">@lang('partner.banner')</span>
+        @if(env('APP_URL') != 'https://app.crestem.ong/')
+            <span class="text-sm font-medium text-gray-600">@lang('partner.banner')</span>
+        @endif
         <div class="flex flex-wrap items-center justify-end flex-1 gap-4 py-5 lg:gap-x-6 sm:flex-initial">
             @foreach ($partners as $partner)
                 @if ($partner->url)
@@ -10,7 +12,11 @@
                         rel="noopener"
                         class="inline-flex hover:ring-1 ring-primary focus:outline-none focus:ring-2 ring-offset-2 px-2.5 py-1.5 shrink-0"
                         title="{{ $partner->name }}">
-                        <img class="object-contain h-12" src="{{ $partner->logo }}" alt="{{ $partner->name }}">
+                        @if(env('APP_URL') != 'https://app.crestem.ong/')
+                            <img class="object-contain h-[80px]" src="{{ $partner->logo }}" alt="{{ $partner->name }}">
+                        @else
+                            <img class="object-contain h-12" src="{{ $partner->logo }}" alt="{{ $partner->name }}">
+                        @endif
                     </a>
                 @else
                     <span class="inline-flex shrink-0">
