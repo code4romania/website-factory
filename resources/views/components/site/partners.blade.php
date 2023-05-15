336b2ce46fcc71531d73f9006aa31305291a6ccf
Lupu Gheorghe <46172059+gheorghelupu17@users.noreply.github.com>
2023-05-16 00:17:15 +0300

    Fix for crestem ong

--- resources/views/components/site/partners.blade.php
+++ resources/views/components/site/partners.blade.php
@@ -1,6 +1,6 @@
 <aside class="border-b">
     <div class="container flex items-center gap-4 sm:justify-end md:gap-x-8">
-        @if(env('APP_URL') != 'https://app.crestem.ong/')
+        @if(env('APP_URL') != 'https://crestem.ong')
             <span class="text-sm font-medium text-gray-600">@lang('partner.banner')</span>
         @endif
         <div class="flex flex-wrap items-center justify-end flex-1 gap-4 py-5 lg:gap-x-6 sm:flex-initial">
@@ -12,7 +12,7 @@
                         rel="noopener"
                         class="inline-flex hover:ring-1 ring-primary focus:outline-none focus:ring-2 ring-offset-2 px-2.5 py-1.5 shrink-0"
                         title="{{ $partner->name }}">
-                        @if(env('APP_URL') != 'https://app.crestem.ong/')
+                        @if(env('APP_URL') == 'https://crestem.ong')
                             <img class="object-contain h-[80px]" src="{{ $partner->logo }}" alt="{{ $partner->name }}">
                         @else
                             <img class="object-contain h-12" src="{{ $partner->logo }}" alt="{{ $partner->name }}">
