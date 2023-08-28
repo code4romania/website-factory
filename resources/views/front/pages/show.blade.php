6b6c1d3addef8567db7d2555991bc8c6a637f32c
Andrei Ioniță <hi@andrei.io>
2023-08-28 15:27:00 +0100

    fix: nested navigation alignment

--- resources/views/front/pages/show.blade.php
+++ resources/views/front/pages/show.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div @class(['container', 'flex flex-wrap gap-4'])>
+    <div class="container flex flex-wrap items-start gap-4">
         <x-nested-navigation :model="$page" />
 
         <x-blocks :model="$page" :container="false" class="flex-1" />
