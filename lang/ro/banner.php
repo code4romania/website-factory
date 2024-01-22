98a2403664139c90fc080b1e07a167627b1c7436
Olivia Vereha <olivia.vereha@commitglobal.org>
2024-01-22 20:20:03 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: Website Factory/banner
    Translate-URL: https://translate.commitglobal.org/projects/website-factory/banner/ro/

--- lang/ro/banner.php
+++ lang/ro/banner.php
@@ -1,19 +1,12 @@
 <?php
-
-declare(strict_types=1);
-
 return [
-
     'internal' => 'O soluție :organization.',
-    'external' => 'Website construit cu Website Factory: :edition, soluție dezvoltată de :organization.',
-
+    'external' => 'Website construit cu Website Factory: :edition, o soluție dezvoltată de :organization.',
     'edition' => [
         'primarie' => 'Primărie',
         'government' => 'Guvern',
         'ong' => 'ONG',
         'develop' => 'develop',
     ],
-
     'government' => 'Website creat cu Website Factory: Guvern. O soluție dezvoltată de Asociația Code for Romania (Commit Global) și pusă gratuit la dispoziția administrației publice.',
-
 ];
