f3b5ec635777f72166ed04c3125c76e8b94b49f6
Tudor <tudor.amariei@commitglobal.org>
2024-01-30 16:14:12 +0200

    Add Georgian (KA) translations (#582)

---
+++ help/content/ong/ka/04-Content/01-intro.md
@@ -0,0 +1,11 @@
+---
+title: "კონტენტის მართვა"
+video: true
+---
+
+ამ სექციაში თქვენ იხილავთ ინფორმაციას პლატფორმის კონტენტის შექმნის ყველა სფეროს შესახებ.
+
+
+<div class="aspect-w-16 aspect-h-9">
+  <iframe src="https://www.youtube.com/embed/tBqtnFOfRYY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
---
+++ lang/ka/banner.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'internal' => ':organization გამოსავალი.',
+    'external' => 'ვებ–გვერდი შექმნილია Website Factory-ის დახმარებით: :edition, გამოსავალი, რომელიც შემუშავებულია :organization-ის მიერ.',
+
+    'edition' => [
+        'primarie' => 'ქალაქის მერია',
+        'government' => 'მთავრობა',
+        'ong' => 'არასამთავრობო ორგანიზაცია',
+        'develop' => 'განვითარება',
+    ],
+
+    'government' => 'ვებსაიტი შექმნილი Website Factory: Government-ის დახმარებით. გამოსავალი შექმნილია Code for Romania (Commit Global)-ის მიერ და ხელმისაწვდომია საჯარო ადმინისტრაციისთვის უფასოდ.',
+
+];
