710ed3d3076f1c400f59b5f0ab6196b39d88ce6f
Andrei Ioniță <hi@andrei.io>
2024-02-24 10:35:00 +0000

    fix: local sail dev environment (#629)
    * fix: update sail php version
    
    * fix: make vite's detectTls opt-in
    
    * docs: update readme link

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,48 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import manifestSRI from 'vite-plugin-manifest-sri';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            refresh: true,
-            detectTls: 'wf.test',
-            input: [
-                'resources/css/admin.css',
-                'resources/js/admin.js',
-                'resources/css/public.css',
-                'resources/js/public.js',
-            ],
-        }),
-        manifestSRI(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, import.meta.dirname);
+
+    return {
+        plugins: [
+            laravel({
+                refresh: true,
+                detectTls: env.VITE_DETECT_TLS || null,
+                input: [
+                    'resources/css/admin.css',
+                    'resources/js/admin.js',
+                    'resources/css/public.css',
+                    'resources/js/public.js',
+                ],
+            }),
+            manifestSRI(),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+            createSvgIconsPlugin({
+                iconDirs: [
+                    path.resolve(process.cwd(), 'node_modules/remixicon/icons'),
+                    path.resolve(process.cwd(), 'resources/svg'),
+                ],
+                symbolId: 'icon-[name]',
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': '/resources/js',
+                '~': '/resources',
+                '%': '/lang',
             },
-        }),
-        createSvgIconsPlugin({
-            iconDirs: [
-                path.resolve(process.cwd(), 'node_modules/remixicon/icons'),
-                path.resolve(process.cwd(), 'resources/svg'),
-            ],
-            symbolId: 'icon-[name]',
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': '/resources/js',
-            '~': '/resources',
-            '%': '/lang',
-        },
-    },
+        }
+    };
 });
