1efcb790c4ee67df9763539a016ad20170a987d0
Cristian Boarna <cristian.boarna@gmail.com>
2024-04-30 05:27:29 +0300

    feat(footer): added footer disclaimer, report issue links, Closes code4romania/website-factory#702

--- resources/views/components/site/banner.blade.php
+++ resources/views/components/site/banner.blade.php
@@ -4,11 +4,38 @@
             href="https://www.commitglobal.org"
             target="_blank"
             rel="noopener"
-            class="flex items-center w-full max-w-2xl gap-3 text-sm hover:text-blue-600 focus:text-blue-600 focus:outline-0 hover:underline focus:underline"
+            class="flex items-center max-w-2xl pr-3 text-sm hover:text-blue-600 focus:text-blue-600 focus:outline-0 hover:underline focus:underline"
         >
             <x-icon-commitglobal class="h-6 sm:h-8 shrink-0" />
-
-            <span>{{ $text }}</span>
         </a>
+        <div class="flex flex-col text-sm">
+            <a
+                class="hover:text-blue-600 focus:text-blue-600 focus:outline-0 hover:underline focus:underline"
+                href="https://www.commitglobal.org"
+                target="_blank"
+                rel="noopener"
+            >
+                {{ $text }}
+            </a>
+            <div class="flex gap-1 font-bold">
+                <a
+                    class="hover:underline focus:underline"
+                    href="https://www.commitglobal.org/en/disclaimer-website-factory"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    Disclaimer
+                </a>
+                <span>|</span>
+                <a
+                    class="hover:underline focus:underline"
+                    href="https://www.commitglobal.org/en/forms/report-website-factory"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    Report an issue
+                </a>
+            </div>
+        </div>
     </div>
 </aside>
