d3a8d592a7afb8e7acd67a17e1a77c0fab106fba
Andrei Ioniță <hi@andrei.io>
2024-05-13 20:08:27 +0300

    wip

---
+++ database/migrations/2024_05_13_180708_make_directory_column_nullable_in_media_table.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->string('directory')->nullable()->change();
+        });
+    }
+};
