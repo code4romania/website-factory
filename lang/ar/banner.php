5eb125d25287a436313a2b1d1641dd5f4c2eb9f1
Weblate <noreply@weblate.org>
2024-09-02 09:42:18 +0000

    Update translation files
    Updated by "Squash Git commits" hook in Weblate.
    
    Update translation files
    
    Updated by "Squash Git commits" hook in Weblate.
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.commitglobal.org/projects/website-factory/banner/
    Translate-URL: https://translate.commitglobal.org/projects/website-factory/validation/
    Translation: Website Factory/banner
    Translation: Website Factory/validation

---
+++ lang/ar/banner.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'edition' => [
+        'government' => 'حكومة',
+        'ong' => 'منظمة غير حكومية',
+        'develop' => 'يطور',
+        'primarie' => 'مبنى البلدية',
+    ],
+    'government' => 'تم إنشاء الموقع باستخدام Website Factory: Government. وهو حل طورته Code for Romania (Commit Global) وتم توفيره مجانًا للإدارة العامة.',
+    'report_issue' => 'إبلاغ عن مشكلة',
+    'internal' => 'أ :organization المنظمة.',
+    'external' => 'تم إنشاء الموقع بواسطة مصنع الموقع :edition، حل تم تطويره من قبل :organization.',
+    'disclaimer' => 'بيان إخلاء مسؤولية',
+];
