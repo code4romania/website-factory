3e0d8d5c96cfbd709b5f7e2cd3fc86d8abfa6230
Weblate <noreply@weblate.org>
2024-09-02 09:42:18 +0000

    Update translation files
    Updated by "Squash Git commits" hook in Weblate.
    
    Update translation files
    
    Updated by "Squash Git commits" hook in Weblate.
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.commitglobal.org/projects/website-factory/banner/
    Translate-URL: https://translate.commitglobal.org/projects/website-factory/validation/
    Translation: Website Factory/banner
    Translation: Website Factory/validation

---
+++ lang/pt/email.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'welcome' => [
+        'subject' => 'A tua nova conta',
+        'greeting' => 'Olá :name!',
+        'intro' => 'Estás a receber este e-mail porque uma nova conta foi criada para ti no :app',
+    ],
+];
---
+++ lang/pt/error.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    401 => [
+        'title' => 'Não autorizado',
+        'message' => 'Desculpa, não tens autorização para aceder à esta página.',
+    ],
+    403 => [
+        'title' => 'Proibido',
+        'message' => 'Desculpa, é-te proibido aceder à esta página.',
+    ],
+    404 => [
+        'title' => 'Não encontrado',
+        'message' => 'Desculpa, a página que estás a procurar não foi encontrada.',
+    ],
+    419 => [
+        'title' => 'Sessão expirada',
+        'message' => 'Desculpa, a tua sessão expirou. Por favor atualiza e tenta novamente.',
+    ],
+    429 => [
+        'title' => 'Demasiados Pedidos',
+        'message' => 'Desculpa, estás a fazer demasiados pedidos aos nossos servidores.',
+    ],
+    500 => [
+        'title' => 'Erro de Servidor Interno',
+        'message' => 'Eh pá, algo não funcionou nos nossos servidores.',
+    ],
+    503 => [
+        'title' => 'Serviço Indisponível',
+        'message' => 'Desculpa, estamos a fazer manutenção. Por favor volta em breve.',
+    ],
+];
