b9e1e5b7ad926a2657b00a2ebb12572096c5a89a
Weblate <noreply@weblate.org>
2024-10-10 17:45:21 +0000

    Added translation using Weblate (Spanish)
    Added translation using Weblate (Spanish)
    
    Added translation using Weblate (Spanish)
    
    Added translation using Weblate (Spanish)
    
    Added translation using Weblate (Spanish)
    
    Co-authored-by: Languages add-on <noreply-addon-languages@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>

---
+++ lang/es/validation.php
@@ -0,0 +1,152 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+    'accepted' => '',
+    'accepted_if' => '',
+    'active_url' => '',
+    'after' => '',
+    'after_or_equal' => '',
+    'alpha' => '',
+    'alpha_dash' => '',
+    'alpha_num' => '',
+    'array' => '',
+    'before' => '',
+    'before_or_equal' => '',
+    'between' => [
+        'array' => '',
+        'file' => '',
+        'numeric' => '',
+        'string' => '',
+    ],
+    'boolean' => '',
+    'confirmed' => '',
+    'current_password' => '',
+    'date' => '',
+    'date_equals' => '',
+    'date_format' => '',
+    'declined' => '',
+    'declined_if' => '',
+    'different' => '',
+    'digits' => '',
+    'digits_between' => '',
+    'dimensions' => '',
+    'distinct' => '',
+    'doesnt_end_with' => '',
+    'doesnt_start_with' => '',
+    'email' => '',
+    'ends_with' => '',
+    'enum' => '',
+    'exists' => '',
+    'file' => '',
+    'filled' => '',
+    'gt' => [
+        'array' => '',
+        'file' => '',
+        'numeric' => '',
+        'string' => '',
+    ],
+    'gte' => [
+        'array' => '',
+        'file' => '',
+        'numeric' => '',
+        'string' => '',
+    ],
+    'image' => '',
+    'in' => '',
+    'in_array' => '',
+    'integer' => '',
+    'ip' => '',
+    'ipv4' => '',
+    'ipv6' => '',
+    'json' => '',
+    'lt' => [
+        'array' => '',
+        'file' => '',
+        'numeric' => '',
+        'string' => '',
+    ],
+    'lte' => [
+        'array' => '',
+        'file' => '',
+        'numeric' => '',
+        'string' => '',
+    ],
+    'mac_address' => '',
+    'max' => [
+        'array' => '',
+        'file' => '',
+        'numeric' => '',
+        'string' => '',
+    ],
+    'mimes' => '',
+    'mimetypes' => '',
+    'min' => [
+        'array' => '',
+        'file' => '',
+        'numeric' => '',
+        'string' => '',
+    ],
+    'multiple_of' => '',
+    'not_in' => '',
+    'not_regex' => '',
+    'numeric' => '',
+    'password' => [
+        'letters' => '',
+        'mixed' => '',
+        'numbers' => '',
+        'symbols' => '',
+        'uncompromised' => '',
+    ],
+    'present' => '',
+    'prohibited' => '',
+    'prohibited_if' => '',
+    'prohibited_unless' => '',
+    'prohibits' => '',
+    'regex' => '',
+    'required' => '',
+    'required_array_keys' => '',
+    'required_if' => '',
+    'required_unless' => '',
+    'required_with' => '',
+    'required_with_all' => '',
+    'required_without' => '',
+    'required_without_all' => '',
+    'same' => '',
+    'size' => [
+        'array' => '',
+        'file' => '',
+        'numeric' => '',
+        'string' => '',
+    ],
+    'starts_with' => '',
+    'string' => '',
+    'timezone' => '',
+    'unique' => '',
+    'uploaded' => '',
+    'url' => '',
+    'uuid' => '',
+    'custom' => [
+        'attribute-name' => [
+            /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+            'rule-name' => '',
+        ],
+    ],
+];
